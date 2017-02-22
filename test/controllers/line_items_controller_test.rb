7e20a9df84d43d5bf8a7fdfe9ad6674ba9501494
Dhruv Chandarana <chandaranadhruv24@gmail.com>
2017-02-22 20:42:51 +0000

    First

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,7 +20,11 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
+    follow_redirect!
+
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Programming Ruby 1.9"
+  
   end
 
   test "should show line_item" do
